5429f77039ebc07aa566862bfb8efeb74775ff3e
Wassim Chegham <github@wassim.dev>
2020-02-28 19:56:26 +0100

    chore: remove old function app

--- functions/azurefunctionbazel-5816/httpTrigger/index.ts
+++
@@ -1,9 +0,0 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-        context.res = {
-            body: "Hello Bazel"
-        };
-};
-
-export default httpTrigger;
